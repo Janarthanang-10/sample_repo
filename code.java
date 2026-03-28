765d3584932eaeb0568e6cd836988de4cc3e0622
Janarthanang-10 <janajana22789@gmail.com>
2026-03-28 12:10:51 +0530

    fix: changing

--- code.java
+++ code.java
@@ -2,5 +2,6 @@ class java{
     public static void main(String[] args){
         System.out.print("Hello World");
         System.out.print("Kavin's java code");
+        system.out.print("bye")
     }
 }
